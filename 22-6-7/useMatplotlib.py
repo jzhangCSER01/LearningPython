a415ff22e5cd7955aa46b69ddb8d01ada975eca8
jzhangCSER01 <1760616780@qq.com>
2022-06-08 19:23:30 +0800

    Update useMatplotlib.py

--- 22-6-7/useMatplotlib.py
+++ 22-6-7/useMatplotlib.py
@@ -1,12 +1,18 @@
-'''
+
 import matplotlib.pyplot as plt #install package matplotlib
 import matplotlib as mpl
-myfont = mpl.font_manager.FontProperties(fname='C:/Windows/Fonts/msyh.ttf')
+#myfont = mpl.font_manager.FontProperties(fname='C:/Windows/Fonts/msyh.ttf')
 plt.plot([-2, 2, 3, 4, 5], 'r', label = u'第一条曲线')   #颜色为红色的曲线
 plt.plot([1, 2, 3, 8, 5], 'b', label = u'第二条曲线')    #颜色为蓝色
 #plt.legend()的作用：在plt.plot() 定义后plt.legend() 会显示该 label 的内容，否则会报error: No handles with labels found to put in legend.
 plt.legend()
+plt.grid(True)
+plt.axis([0, 5, -3, 9]) #坐标轴的最大值与最小值
+plt.xlabel(u'X轴坐标')    # 坐标轴标签
+plt.ylabel(u'Y轴坐标')
+plt.title(u'matplotlib演示图')
 plt.show()
+
 '''
 import matplotlib.pyplot as plt
 
@@ -21,3 +27,4 @@ plt.legend()    # 将样例显示出来
 plt.plot()
 plt.show()
 # 测试一次
+'''
