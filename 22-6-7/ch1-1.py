e163d515b7c58f3c4f7b079c99cf79667c9fb44d
jzhangCSER01 <1760616780@qq.com>
2022-06-08 20:30:53 +0800

    Update ch1-1.py

--- 22-6-7/ch1-1.py
+++ 22-6-7/ch1-1.py
@@ -2,11 +2,14 @@
 import matplotlib.pyplot as plt #install package matplotlib
 import matplotlib as mpl
 #myfont = mpl.font_manager.FontProperties(fname='C:/Windows/Fonts/msyh.ttf')
+# 加这个两句 可以显示中文
+plt.rcParams['font.sans-serif'] = [u'SimHei']
+plt.rcParams['axes.unicode_minus'] = False
 plt.plot([-2, 2, 3, 4, 5], 'r', label = u'第一条曲线')   #颜色为红色的曲线
 plt.plot([1, 2, 3, 8, 5], 'b', label = u'第二条曲线')    #颜色为蓝色
 #plt.legend()的作用：在plt.plot() 定义后plt.legend() 会显示该 label 的内容，否则会报error: No handles with labels found to put in legend.
 plt.legend()
-plt.grid(True)
+plt.grid(True)  #显示网格
 plt.axis([0, 5, -3, 9]) #坐标轴的最大值与最小值
 plt.xlabel(u'X轴坐标')    # 坐标轴标签
 plt.ylabel(u'Y轴坐标')
