4863c3a243f93f339f80c9f7675efc13902d8f9f
jzhangCSER01 <68910270+jzhangCSER01@users.noreply.github.com>
2022-08-25 16:34:59 +0800

    Delete 22-6-7 directory

--- 22-6-7/ch3-1.py
+++
@@ -1,8 +0,0 @@
-'''
-单分支结构 if
-'''
-
-a = ['mike', 'mary', 'john', 'tom', 'jenny', 'herry']
-b = input('please input your name:')
-if(b in a):
-    print('欢迎: %s' %b)
--- 22-6-7/ch3-2.py
+++
@@ -1,9 +0,0 @@
-'''
-单分支结构 检测用户是否正确输入用户名和密码
-'''
-
-a = dict((['mike', '001'], ['mary', '002'], ['john', '003'], ['tom', '004'], ['jenny', '005'], ['herry', '006']))
-b = input('please input your name:')
-c = input('please input your password:')
-if(b in a) and c == a[b]:
-    print('欢迎: %s' %b)
